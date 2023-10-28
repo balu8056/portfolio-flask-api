88afe71065ce4f8bd76ef8735f748b40c024545b
Balasubramani R <46841670+balu8056@users.noreply.github.com>
2023-10-28 23:10:52 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -6,3 +7,7 @@ app = Flask(__name__)
 @app.get('/')
 def home():
     return {'greet': 'welcome'}
+
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8080)
